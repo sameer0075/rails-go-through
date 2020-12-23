238ad84b6fccaa9f6b12fd6d9b10ed110a4f9cd2
sameer0075 <sameerbutt151@gmail.com>
2020-12-23 17:12:27 +0500

    portfolio edit update show and delete actions created

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -8,16 +8,44 @@ def new
 	@portfolio = Portfolio.new
 end
 
+def show
+	@portfolio = Portfolio.find(params[:id])
+end
+
 def create
     @portfolio = Portfolio.new(params.require(:portfolio).permit(:title,:subtitle,:body))
 
     respond_to do |format|
       if @portfolio.save
-        format.html { redirect_to portfolios_path, notice: 'Blog was successfully created.' }
+        format.html { redirect_to @portfolio, notice: 'Portfolio was successfully created.' }
   
       else
         format.html { render :new }
       end
+    end
+  end
+
+  def edit
+  	@portfolio = Portfolio.find(params[:id])
+  end
+
+
+  def update
+  	@portfolio = Portfolio.find(params[:id])
+    respond_to do |format|
+      if @portfolio.update(params.require(:portfolio).permit(:title,:subtitle,:body))
+        format.html { redirect_to portfolio_path, notice: 'Portfolio was successfully updated.' }
+      else
+        format.html { render :edit }
+      end
+    end
+  end
+
+  def destroy
+  	@portfolio = Portfolio.find(params[:id])
+    @portfolio.destroy
+    respond_to do |format|
+      format.html { redirect_to portfolios_url, notice: 'Portfolio was successfully destroyed.' } 
     end
   end
 
