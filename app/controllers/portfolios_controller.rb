01b78116369b2787d40210f2ece6d6ef6c3ed935
sameer0075 <sameerbutt151@gmail.com>
2020-12-23 15:37:01 +0500

    portfolio new and create action created

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -4,4 +4,21 @@ def index
 	@portfolio = Portfolio.all
 end
 
+def new
+	@portfolio = Portfolio.new
+end
+
+def create
+    @portfolio = Portfolio.new(params.require(:portfolio).permit(:title,:subtitle,:body))
+
+    respond_to do |format|
+      if @portfolio.save
+        format.html { redirect_to portfolios_path, notice: 'Blog was successfully created.' }
+  
+      else
+        format.html { render :new }
+      end
+    end
+  end
+
 end
