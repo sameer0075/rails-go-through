7999ff675a22832ca3a747f1567362e91520ec04
sameer0075 <sameerbutt151@gmail.com>
2020-12-25 18:06:07 +0500

    custom method

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,5 +1,5 @@
 class BlogsController < ApplicationController
-  before_action :set_blog, only: [:show, :edit, :update, :destroy]
+  before_action :set_blog, only: [:show, :edit, :update, :destroy,:toggle_status]
 
   # GET /blogs
   # GET /blogs.json
@@ -16,6 +16,15 @@ class BlogsController < ApplicationController
   def new
     @blog = Blog.new
   end
+
+def toggle_status
+  if @blog.draft?
+    @blog.published!
+  elsif @blog.published?
+    @blog.draft!
+  end
+  redirect_to blogs_url, notice: 'Status Has Been Updated.'
+end
 
   # GET /blogs/1/edit
   def edit
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,11 @@ Rails.application.routes.draw do
   get '/about',to:'pages#about'
    get '/contact',to:'pages#contact'
   get 'pages/contact'
-  resources :blogs
+  resources :blogs do
+  	member do
+  		get :toggle_status
+  	end
+  end
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
